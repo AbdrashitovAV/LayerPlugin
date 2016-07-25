1da8b0905d31c84cd23451fd1d4a32067f48c23a
abdrashitovav <abdrashitovav@gmail.com>
2016-07-25 22:46:11 +0700

    Fixed problem with height

---
+++ LayerPlugin.Views/BasicViews/DoubleInput.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayerPlugin.Views.BasicViews
+{
+    public partial class DoubleInput : UserControl
+    {
+        public String Value
+        {
+            get { return (String)this.GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(String), typeof(BasicViews.DoubleInput), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public String Caption
+        {
+            get { return (String)this.GetValue(CaptionProperty); }
+            set { this.SetValue(CaptionProperty, value); }
+        }
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(String), typeof(BasicViews.DoubleInput), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        
+        public DoubleInput()
+        {
+            InitializeComponent();
+
+//            (this.Content as FrameworkElement).DataContext = this;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void SetValueDP(DependencyProperty property, object value, [System.Runtime.CompilerServices.CallerMemberName] string p = null)
+        {
+            SetValue(property, value);
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+}
--- LayerPlugin.Views/BasicViews/HeightView.xaml.cs
+++ LayerPlugin.Views/BasicViews/HeightView.xaml.cs
@@ -1,9 +1,19 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace LayerPlugin.Views.BasicViews
 {
     public partial class HeightView : UserControl
     {
+        public Double Height
+        {
+            get { return (Double)this.GetValue(HeightProperty); }
+            set { SetValue(HeightProperty, value); }
+        }
+        public static readonly DependencyProperty HeightProperty = DependencyProperty.Register("Height", typeof(Double), typeof(BasicViews.HeightView), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
         public HeightView()
         {
             InitializeComponent();
