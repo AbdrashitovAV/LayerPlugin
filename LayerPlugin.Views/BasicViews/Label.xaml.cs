aaf68418ccf62bd47a7cee145f2caabd1fe51a44
abdrashitovav <abdrashitovav@gmail.com>
2016-07-25 21:45:28 +0700

    Object type moved to seaparete view

---
+++ LayerPlugin.Views/BasicViews/Label.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayerPlugin.Views.BasicViews
+{
+    /// <summary>
+    /// Interaction logic for Label.xaml
+    /// </summary>
+    public partial class Label : UserControl
+    {
+        public String ObjectType
+        {
+            get { return (String)this.GetValue(ObjectTypeProperty); }
+            set { this.SetValue(ObjectTypeProperty, value); }
+        }
+        public static readonly DependencyProperty ObjectTypeProperty = DependencyProperty.Register("ObjectType", typeof(String), typeof(BasicViews.Label));
+
+        public Label()
+        {
+            InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+    }
+}
